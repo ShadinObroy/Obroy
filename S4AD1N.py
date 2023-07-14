6d1e6afae9f4fa605149ee670eee4a3deab3f1a5
S4AD1N <102572848+ShadinObroy@users.noreply.github.com>
2023-07-14 17:46:52 +0600

    Update S4AD1N.py

--- S4AD1N.py
+++ S4AD1N.py
@@ -162,7 +162,7 @@ def main_apv():
         time.sleep(3.5)
         tks = 'Dear%20Admin,%20Please%20Approved%20My%20Token%20To%20Premium%20% 20% 20%20%20My%20%20Key%20%20:%20'+ak+''+myid+''+imt
 
-        os.system('am start https://wa.me/+88018122775755?text=' + tks)
+        os.system('am start https://wa.me/+8801863231665?text=' + tks)
 
         
 
